5875bec89983bce7a77f014c2d3002fc1df13c7d
AndrewCz <andrewcz@neomailbox.net>
2017-02-23 00:58:36 -0500

    post-compile fixes

--- config/includes.chroot/usr/local/bin/menu-to-cli.sh
+++ config/includes.chroot/usr/local/bin/menu-to-cli.sh
@@ -9,45 +9,25 @@ OUTFILE="${HOME}/.tmuxinator/${CLI_PROGRAM}.yml"   # Name of the file to generat
 # -----------------------------------------------------------
 # 'Here document' containing the body of the config file
 # Generated only when one does not already exist
-if [[ ! -e ~/.tmuxinator/${CLI_PROGRAM} ]]; then
-    if [[ ${ARGUMENTS} =~ "-h" ]]; then
-        # All space indented in the here document would be reflected in the
-        # resulting file, therefore, indentation needs to be eliminated for that block
-        (
-        cat << 'EOF'
+if [[ -e ${OUTFILE} ]]; then
+    # All space indented in the here document would be reflected in the
+    # resulting file, therefore, indentation needs to be eliminated for that block
+    (
+    cat << EOF
 name: ${CLI_PROGRAM}
 root: ~/
 windows:
   - cli:
-    layout: main-vertical
-    panes:
-      - ${CLI_PROGRAM} ${ARGUMENTS} | less
+      layout: main-vertical
+      panes:
+        - ${CLI_PROGRAM} ${ARGUMENTS}
 EOF
         ) > "${OUTFILE}"
-    else
-        (
-        cat <<'EOF'
-name: ${CLI_PROGRAM}
-root: ~/
-windows:
-  - cli:
-    layout: main-vertical
-    panes:
-      - ${CLI_PROGRAM} ${ARGUMENTS}
-EOF
-        ) > "${OUTFILE}"
-    fi
-    chmod 644 "${OUTFILE}"
-else
-    notify-send "ERROR" "Problem in creating file: \"$OUTFILE\"\n\
-    You can call the command manually with\n\
-    ${CLI_PROGRAM} ${ARGUMENTS}\n\
-    You can also report this error to us using 'whisperback'"
-    exit 1
+    notify-send "Success" "Successfully created:\n${OUTFILE}"
 fi
 
-terminator -T "${CLI_PROGRAM}" -e bash -c "tmuxinator start ${CLI_PROGRAM}" \
-    || notify-send "ERROR" "Problem in running ${CLI_PROGRAM} in the terminal\n\
-        You can call the command manually with\n\
-        ${CLI_PROGRAM} ${ARGUMENTS}\n\
-        You can also report this error to us using 'whisperback'"
+terminator -T "${CLI_PROGRAM}" -e "tmuxinator start ${CLI_PROGRAM}" || \
+notify-send "ERROR" "Problem in running ${CLI_PROGRAM} in the terminal\n\
+You can call the command manually with\n\
+${CLI_PROGRAM} ${ARGUMENTS}\n\
+You can also report this error to us using 'whisperback'"
